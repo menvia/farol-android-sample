2329a58d0addf6c04791d991e82c45942ff80e33
Rodrigo Ra <rodrigorcomp@gmail.com>
2016-02-18 14:52:52 -0200

    try fix android 6 permissions

--- app/src/main/java/com/menvia/farolsample/BeaconActivity.java
+++ app/src/main/java/com/menvia/farolsample/BeaconActivity.java
@@ -1,6 +1,12 @@
 package com.menvia.farolsample;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
 
 import com.menvia.farolsample.fragments.BeaconFragment;
 import com.menvia.farolsample.utils.SingleFragmentActivity;
@@ -8,6 +14,8 @@ import com.menvia.farolsample.utils.SingleFragmentActivity;
 import org.altbeacon.beacon.Beacon;
 
 public class BeaconActivity extends SingleFragmentActivity {
+
+
     @Override
     protected Fragment createFragment() {
         return BeaconFragment.newInstance((Beacon) getIntent().getParcelableExtra(BeaconFragment.EXTRA_BEACON));
